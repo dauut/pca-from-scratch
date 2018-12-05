501f6917f12995f616b4351990d453b03ec84b0c
dauut <davutucar@gmail.com>
2018-12-04 22:19:43 -0800

    Initial commit completed project

---
+++ compress.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pca
+
+from sklearn.decomposition import PCA
+
+TRAINING_DATA = "Data/Train"
+TEST_DATA = "Data/Test"
+OUTPUT_PATH = "Output"
+
+
+def compress_images(DATA, k):
+
+    # create output directory
+    if not os.path.exists(OUTPUT_PATH):
+        os.makedirs(OUTPUT_PATH)
+
+    faces = []
+    filenames = []
+    for face in DATA:
+        filenames.append(face[1])
+        faces.append(face[2])
+
+    # we convert each image to a feature (column)
+    faces = np.asarray(faces)
+    faces = np.transpose(faces)
+
+    # pca processes
+    Z = pca.compute_Z(faces)
+    COV = pca.compute_covariance_matrix(Z)
+    L, PCS = pca.find_pcs(COV)
+    Z_star = pca.project_data(Z, PCS, L, k, 0)
+
+    # we use components for compressing files, we need to reduce component count
+    component_matrix = np.delete(PCS, range(k, PCS.shape[1]), axis=1)
+    Ut = component_matrix.T
+    X_compressed = np.dot(Z_star, Ut)
+
+
+    # write all images
+    for ftr_ind in range(faces.shape[1]):
+        img = np.reshape(X_compressed[:,ftr_ind], (60, 48))
+        plt.imsave(OUTPUT_PATH + "/" + filenames[ftr_ind] + "_img.jpg", img, cmap="gray")
+
+    print()
+
+
+def load_data(input_dir):
+
+    # collect all data file names
+    file_name_list = []
+    for root, dirs, files in os.walk(input_dir):
+        for filename in files:
+            file_name_list.append(filename)
+
+    # read each image and properties
+    image_list = []
+    for i in range(len(file_name_list)):
+        file_path = input_dir
+        file_path += "/"
+        filename = file_name_list[i]
+        file_path += filename
+        image = plt.imread(file_path)
+        float_image = np.asarray(image.flatten(), dtype=float)
+        image_list.append((image, filename, float_image))
+
+    return image_list
+
+
+X = load_data(TRAINING_DATA)
+compress_images(X, 2000)
---
+++ pca.py
@@ -0,0 +1,77 @@
+import numpy as np
+from numpy import linalg as LA
+
+
+def compute_Z(X, centering=True, scaling=True):
+    # copying X because need a float type
+    Z = X.astype(float)
+
+    # shapes
+    rows = Z.shape[0]
+    cols = Z.shape[1]
+
+    if centering:
+        # means calculation for centering
+        means = X.mean(0)
+        for i in range(rows):
+            for j in range(cols):
+                Z[i, j]= Z[i, j] - means[j]
+
+    # scaling with std dev
+    if scaling:
+        std = np.std(Z, axis=0)
+        for i in range(cols):
+            for j in range(rows):
+                Z[j][i] = Z[j][i] / std[i] # standardize every feature
+
+    return Z
+
+
+def compute_covariance_matrix(Z):
+    return Z.T.dot(Z)
+
+
+def find_pcs(COV):
+    eigenValues, eigenVectors = LA.eig(COV)
+    idx = eigenValues.argsort()[::-1]
+    eigenValues = eigenValues[idx]
+    eigenVectors = eigenVectors[:, idx]
+
+    return eigenValues, eigenVectors
+
+
+def project_data(Z, PCS, L, k, var):
+    
+    eigen_pairs = [(np.abs(L[i]), PCS[:, i]) for i in range(len(L))]
+    eigen_pairs.sort(key=lambda x: x[0], reverse=True)
+
+    #copy of components could be useful
+    component_matrix = np.copy(PCS)
+    if k != 0:
+        # delete unnecessary components
+        component_matrix = np.delete(component_matrix, range(k, component_matrix.shape[1]), axis=1)
+        Z_star = Z.dot(component_matrix)
+
+    else:
+        print()
+        tot_var = 0
+        eigen_val_index = 0
+        while tot_var < var:
+            tot_var = tot_var + L[eigen_val_index] / np.sum(L)
+            eigen_val_index = eigen_val_index + 1
+        component_count = eigen_val_index
+        component_matrix = np.delete(component_matrix, range(component_count, component_matrix.shape[1]), axis=1)
+        Z_star = Z.dot(component_matrix)
+    return Z_star
+
+
+# X = np.array([[-1, -1], [-1, 1], [1, -1], [1, 1]])
+# X1 = np.array([[-1, -1], [-1, 1], [1, -1], [1, 1]])
+# X = np.array([[1, 1], [2, 7], [3, 3], [4, 4], [5, 5]])
+# X1 = np.array([[1, 1], [2, 7], [3, 3], [4, 4], [5, 5]])
+# Z = compute_Z(X)
+# COV = compute_covariance_matrix(Z)
+# L, PCS = find_pcs(COV)
+# Z_star = project_data(Z, PCS, L, 0, 0.8)
+#
+# print()
